4f8dfe5d9f460ea3ae9b0ad03c16fde84ee1146f
movingnewboy <moovingnewboy@gmail.com>
2025-01-30 13:12:01 +0530

    Update info.py

--- info.py
+++ info.py
@@ -88,9 +88,9 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', 'https://t.me/+hQF-Vl-d
 
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+3D17cyM3F3BjYzg1')
+GRP_LNK = environ.get('GRP_LNK', 't.me/+3D17cyM3F3BjYzg1')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/Tamilan_Rocks')
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 't.me/+3D17cyM3F3BjYzg1') # Support Chat Link Without https:// or @
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'QTL_UPDATES03') # Support Chat Link Without https:// or @
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/QTL_UPDATES03')
 
 # True Or False
