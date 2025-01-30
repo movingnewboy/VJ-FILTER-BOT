499ec8a05459f839c4cd667f0484c3b6c987c2c3
movingnewboy <moovingnewboy@gmail.com>
2025-01-30 10:52:03 +0530

    Update misc.py

--- plugins/misc.py
+++ plugins/misc.py
@@ -1,6 +1,6 @@
-# Don't Remove Credit @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit Tg - @Quality_Bots
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@RockingTamilans
+# Ask Doubt on telegram @tamilian01
 
 import os, logging, time
 from pyrogram import Client, filters, enums
